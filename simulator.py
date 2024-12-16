43b2e3df71b0d0a2cfcfc48f64cc499219f9d9ac
Carson Bring <carsonbring@gmail.com>
2024-12-16 16:31:33 -0600

    Generating output successfully

--- simulator.py
+++ simulator.py
@@ -1,9 +1,10 @@
+from inspect import Attribute
 from rich import print, box
 import threading
 from rich.console import Console
 from rich.table import Table
-from rich.prompt import Prompt
-from utils import list_network_interfaces, wait_for_stop
+from rich.prompt import Prompt, IntPrompt
+from utils import list_network_interfaces, wait_for_stop, run_workflow
 import pyshark
 from pyshark.packet.packet import Packet
 
@@ -14,6 +15,7 @@ class VideoTrafficSimulator:
         self.console = Console()
         self.menu = True
         self.packets = []
+        self.packet_string = ""
 
     def run(self):
         self.display_menu()
@@ -64,18 +66,58 @@ class VideoTrafficSimulator:
             self.console.print(f"[red]Unknown command: {command}[/red]")
 
     def capture_packets(
-        self, interface: str, stop_event: threading.Event, console: Console
+        self,
+        interface: str,
+        stop_event: threading.Event,
+        console: Console,
+        src_address: str,
+        port: int,
     ) -> list[Packet] | None:
         try:
-            capture = pyshark.LiveCapture(interface=interface)
-            for packet in capture.sniff_continuously():
-                if stop_event.is_set():
-                    capture.close()
-                    console.print(
-                        f"[bold green]Packet capture stopped. {len(self.packets)} packets captured.[/bold green]"
-                    )
-                self.packets.append(packet)
-                console.print(f"[green]Packet received: {packet}[/green]")
+            if port != -1:
+                capture = pyshark.LiveCapture(
+                    interface=interface,
+                    bpf_filter=f"udp port {port}",
+                )
+                console.print(
+                    f"[bold blue]Listing to traffic on Port: {port}[/bold blue]"
+                )
+            else:
+                capture = pyshark.LiveCapture(
+                    interface=interface,
+                    bpf_filter="udp",
+                )
+                console.print(
+                    f"[bold blue]Listing to traffic from src address: {src_address}[/bold blue]"
+                )
+
+            print(interface)
+
+            for packet in capture.sniff_continuously(packet_count=50):
+                try:
+                    if stop_event.is_set():
+                        capture.close()
+                        console.print(
+                            f"[bold green]Packet capture stopped. {len(self.packets)} packets captured.[/bold green]"
+                        )
+                        break
+                    if src_address != "":
+                        if packet.ip.src == src_address:
+                            console.print("Packet read")
+                            self.packets.append(packet)
+                    else:
+                        console.print("Packet read")
+                        self.packets.append(packet)
+                except AttributeError as e:
+                    pass
+
+                # console.print(f"[green]Packet received: {packet}[/green]")
+
+            console.print(
+                f"[bold green]Packet capture stopped. {len(self.packets)} packets captured.[/bold green]"
+            )
+
+            capture.close()
         except Exception as e:
             console.print(f"[red]Error during packet capture: {e}[/red]")
 
@@ -90,32 +132,102 @@ class VideoTrafficSimulator:
         )
         if user_input == "exit":
             return
+        interface_name = user_input
+
+        port = -1
+        src_address = ""
+
+        user_input = Prompt.ask(
+            "Port or IP filter?",
+            choices=["port", "ip"],
+            default="exit",
+        )
+        if user_input == "exit":
+            return
+        elif user_input == "ip":
+            user_input = Prompt.ask(
+                "Source IP address to listen for:",
+                default="exit",
+            )
+            if user_input == "exit":
+                return
+            src_address = user_input
+        elif user_input == "port":
+            user_input = IntPrompt.ask(
+                "Port to listen on:",
+                default=-1,
+            )
+            if user_input == -1:
+                return
+            port = user_input
 
         stop_event = threading.Event()
         capture_thread = threading.Thread(
             target=self.capture_packets,
-            args=(
-                user_input,
-                stop_event,
-                self.console,
-            ),
-            daemon=True,
+            args=(interface_name, stop_event, self.console, src_address, port),
+            daemon=False,
         )
         capture_thread.start()
         self.console.print(
-            "[green]PyShark is now listening to network traffic.[/green]"
+            "[green]PyShark is now listening to network traffic. Press Enter to stop[/green]"
         )
         stop_thread = threading.Thread(
             target=wait_for_stop,
             args=(stop_event, capture_thread, self.console),
-            daemon=True,
+            daemon=False,
         )
         stop_thread.start()
+        stop_thread.join()
 
-    def load_network_data(self):
+        user_input = Prompt.ask(
+            "Write result or generate packets?",
+            choices=["write", "gen"],
+            default="exit",
+        )
+        if user_input == "exit":
+            return
+        else:
+            packet_string = "<PacketData>\n"
+            for i, packet in enumerate(self.packets):
+                packet_string = packet_string + f"<Packet{i}>\n"
+                packet_string = packet_string + str(packet) + "\n"
+                packet_string = packet_string + f"</Packet{i}>\n"
+            packet_string = packet_string + "</PacketData>\n"
+            if user_input == "write":
+                with open("packets.txt", "w") as file:
+                    file.write(packet_string)
+                user_input = Prompt.ask(
+                    "Continue..?",
+                    choices=["yes", "no"],
+                    default="no",
+                )
+                if user_input == "no":
+                    return
+                else:
+                    self.run()
+            else:
+                self.packet_string = packet_string
+                self.load_network_data(self.packet_string)
+
+    def load_network_data(self, packet_string=""):
         self.console.print("[bold blue]Loading network data...[/bold blue]")
-        file_path = Prompt.ask("Enter the path to your data file")
-        self.console.print(f"[green]Loaded data from {file_path}.[/green]")
+        if packet_string == "":
+            file_path = Prompt.ask("Enter the path to your data file")
+            with open(file_path) as file:
+                packet_string = file.read()
+            self.console.print(f"[green]Loaded data from {file_path}.[/green]")
+        else:
+            self.console.print("[green]Loaded data from PyShark session.[/green]")
+        run_workflow(self.console, packet_string)
+        user_input = Prompt.ask(
+            "Continue..?",
+            choices=["yes", "no"],
+            default="no",
+        )
+        if user_input == "no":
+            return
+        else:
+            self.run()
 
     def exit_application(self):
         self.console.print("[bold red]Exiting the application. Goodbye![/bold red]")
--- utils.py
+++ utils.py
@@ -3,6 +3,7 @@ import pyshark
 from rich import print
 from rich.console import Console
 import threading
+from autogen import ConversableAgent, ChatResult
 
 
 def list_network_interfaces(console: Console) -> list[str]:
@@ -30,3 +31,67 @@ def wait_for_stop(
     console.print("[bold red]Stopping packet capture...[/bold red]")
     if capture_thread:
         capture_thread.join()
+
+
+def run_workflow(console: Console, packet_string: str):
+    console.print("Running autogen workflow")
+    generator = ConversableAgent(
+        name="Video Traffic Packet Generator",
+        llm_config={
+            "config_list": [
+                {
+                    "model": "llama3.2:3b",
+                    "temperature": 0.6,
+                    "base_url": "http://localhost:11434/v1",
+                    "api_key": "ollama",
+                }
+            ]
+        },
+        system_message="""<Profile> \n You are a cybersecurity expert and have lots of experience dealing with video network traffic. \n</Profile> \n
+        <Goal> \n Your ONLY goal is to generate 3 video traffic packets in the same format as the packets in your context. ONLY RESPOND WITH THE GENERATED PACKETS AND NOTHING ELSE. YOU MUST ONLY RESPOND WITH THREE generated packets in the same format as the ones given to you by the reviewer and they MUST have the same level of detail. \n </Goal> \n
+        """,
+        human_input_mode="NEVER",
+        is_termination_msg=lambda msg: "terminate" in msg["content"].lower(),
+    )
+    reviewer = ConversableAgent(
+        name="Video Traffic Packet Reviewer",
+        llm_config={
+            "config_list": [
+                {
+                    "model": "llama3.2:3b",
+                    "temperature": 0.6,
+                    "base_url": "http://localhost:11434/v1",
+                    "api_key": "ollama",
+                }
+            ]
+        },
+        system_message="""<Profile> \n You are a cybersecurity expert and have lots of experience dealing with video network traffic. \n</Profile> \n
+        <Goal> \n Your goal is to review the Video Traffic Packet Generator's generated packets and ensure they fit with the previous packets in the conversation history. If they do not, tell the Generator how to fix the problem. \n </Goal> \n
+        ONLY SAY 'TERMINATE' if the generated packets look correct. You shouldn't say 'TERMINATE' in any other context""",
+        human_input_mode="NEVER",
+    )
+
+    try:
+        result = reviewer.initiate_chat(
+            generator,
+            message=f"Generate video traffic packets based on the following input: \n<INPUT>\n{packet_string}\n</INPUT>\n",
+        )
+        console.print("[bold green]Workflow completed successfully.[/bold green]")
+
+        write_output(result)
+        console.print(f"[bold blue] RESULT WRITTEN at output.txt[/bold blue]")
+
+    except Exception as e:
+        console.print(
+            f"[bold red]An error occurred during the workflow: {e}[/bold red]"
+        )
+
+
+def write_output(result: ChatResult):
+    with open("output.txt", "w") as file:
+        for i, message in enumerate(result.chat_history[1:]):
+            file.write(f"\n---------------------Message {i}-----------------------\n")
+            file.write(
+                f"++++++++++++++++++Agent {message['name']}+++++++++++++++++++++++++++\n"
+            )
+            file.write(message["content"])
